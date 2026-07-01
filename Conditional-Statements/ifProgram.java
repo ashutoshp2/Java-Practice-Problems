f17923b5e30fd684c1733011c2cb42cc4b8df889
Ashutosh Patil <ashutoshpatil1919@gmail.com>
2026-07-01 11:37:03 +0530

    Add ifProgram to check voting eligibility by age

---
+++ Conditional-Statements/ifProgram.java
@@ -0,0 +1,15 @@
+package ConditionalStatements;
+
+import java.util.Scanner;
+
+public class ifProgram {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter age:");
+        int age = sc.nextInt();
+
+        if(age >= 18){
+            System.out.println("Eligible for Voting");
+        }
+    }
+}
